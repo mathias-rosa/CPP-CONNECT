db0685c674fe2ca6a75ab3e315b3954ca2ca4b41
Matro123 <mat.ros@outlook.fr>
2021-07-02 11:59:37 +0400

    database imporovements

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,8 @@ login_manager.login_view = 'login'
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True)
+    surname = db.Column(db.String(25), unique=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
@@ -29,14 +31,16 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
-    username = StringField('', validators=[InputRequired(), Length(min=4, max=15)])
+    username = StringField('', validators=[InputRequired()])
     password = PasswordField('', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('Se rappeller de moi')
 
 class RegisterForm(FlaskForm):
+    name = StringField('', validators=[InputRequired()])
+    surname = StringField('', validators=[InputRequired()])
     email = StringField('', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('', validators=[InputRequired(), Length(min=8, max=80)])
+    password_confirm = PasswordField('', validators=[InputRequired(), Length(min=8, max=80)])
 
 @app.route('/favicon.ico')
 def fav():
@@ -46,13 +50,40 @@ def fav():
 def index():
     return render_template('index.html', year=datetime.date.today().year)
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data == form.password_confirm.data:
+            hashed_password = generate_password_hash(form.password.data, method='sha256')
+            username = form.surname.data.lower() + "." + form.name.data.lower()
+            new_user = User(name=form.surname.data,
+                            surname=form.name.data,
+                            username=username,
+                            email=form.email.data,
+                            password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            user = User.query.filter_by(username=username).first()
+            login_user(user)
+            return redirect(url_for('dashboard'))
+
+        return "<h1>Les mots de passe ne correspondent pas</h1>"
+
+    return render_template('signup.html', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        username = form.username.data
+        if "@" in username:
+            user = User.query.filter_by(email=username).first()
+        else:
+            user = User.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
@@ -62,30 +93,17 @@ def login():
 
     return render_template('login.html', form=form)
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-
-    return render_template('signup.html', form=form)
 
 @app.route('/dashboard/')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
+    return render_template('dashboard.html', current_user=current_user)
+
 
 @app.route('/admin/')
 @login_required
 def admin():
-    return render_template('admin.html', name=current_user.username)
+    return render_template('admin.html', current_user=current_user)
 
 
 @app.route('/logout')
@@ -93,6 +111,7 @@ def admin():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
