c48c81a0f55429cd744e4f0f716fbe9f0d5d82cb
Matro123 <mat.ros@outlook.fr>
2021-08-12 19:28:50 +0400

    gmail

--- app/__init__.py
+++ app/__init__.py
@@ -16,8 +16,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
